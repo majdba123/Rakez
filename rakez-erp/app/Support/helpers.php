b305825e733f9bfc623be7765b1bb5f1f92075e4
Majd Bayer <137396563+majdba123@users.noreply.github.com>
2026-02-07 21:53:14 +0200

    first

--- rakez-erp/app/Support/helpers.php
+++ rakez-erp/app/Support/helpers.php
@@ -1,8 +1,5 @@
 <?php
 
-use Faker\Factory as FakerFactory;
-use Faker\Generator;
-
 if (! function_exists('fake')) {
     /**
      * Get a shared Faker instance.
@@ -10,13 +7,20 @@ if (! function_exists('fake')) {
      * This is intentionally defined as a global helper because multiple seeders
      * call fake() directly.
      */
-    function fake(?string $locale = null): Generator
+    function fake(?string $locale = null)
     {
+        if (!class_exists(\Faker\Factory::class)) {
+            throw new \RuntimeException(
+                'Faker is not installed. To run seeders, install dev dependencies (run `composer install` without `--no-dev`) '
+                . 'or add `fakerphp/faker` to require and update composer.lock.'
+            );
+        }
+
         static $fakers = [];
 
         $locale = $locale ?: config('app.faker_locale', 'en_US');
 
-        return $fakers[$locale] ??= FakerFactory::create($locale);
+        return $fakers[$locale] ??= \Faker\Factory::create($locale);
     }
 }
 
