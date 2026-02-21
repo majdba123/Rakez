0d90a34b301453194784d5a34f9f6ba6a6b2a5b0
Majd Bayer <137396563+majdba123@users.noreply.github.com>
2026-02-22 00:51:50 +0200

    Revert "Update cors.php"
    This reverts commit 65e76c2361813daabecbb0423ecd46563b182860.

--- rakez-erp/config/cors.php
+++ rakez-erp/config/cors.php
@@ -19,9 +19,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-
+        
         'https://www.rakez.com.sa',
-        'rakez.com.sa',
 
         'http://localhost:3000',
         'http://127.0.0.1:3000',
